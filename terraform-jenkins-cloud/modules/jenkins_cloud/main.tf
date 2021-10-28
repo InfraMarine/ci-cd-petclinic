71fd48ade724e07bf6f9382c16b5bcf8f73ebb6b
Usernamen <tvjunnik@gmail.com>
2021-10-28 20:07:42 +0300

    fix

--- terraform-jenkins-cloud/modules/jenkins_cloud/main.tf
+++ terraform-jenkins-cloud/modules/jenkins_cloud/main.tf
@@ -12,7 +12,7 @@ resource "aws_ecs_service" "jenkins" {
   cluster = var.ecs_cluster_id
   task_definition = aws_ecs_task_definition.jenkins.arn
   desired_count = 1
-  deployment_minimum_healthy_percent  = 50
+  deployment_minimum_healthy_percent  = 100
   health_check_grace_period_seconds   = 45
   force_new_deployment                = true
   launch_type                         = "EC2"
